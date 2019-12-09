8c82e77d936a229f204b2379be0f4b516fd2812a
Vaharsolta <vaha-22@mail.ru>
2019-12-09 23:30:38 +0300

    Added migration for table triggers

---
+++ database/migrations/2019_12_09_202804_create_triggers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTriggersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('triggers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('trigger');
+            $table->char('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('triggers');
+    }
+}
