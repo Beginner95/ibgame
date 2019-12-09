482d5b343356f9ad7e47e148aa7df9525b85b776
Vaharsolta <vaha-22@mail.ru>
2019-12-09 23:20:49 +0300

    Added migration for table resources

---
+++ database/migrations/2019_12_09_201635_create_resources_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('resource');
+            $table->char('status', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+}
