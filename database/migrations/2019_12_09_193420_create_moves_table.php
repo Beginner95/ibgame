90817452695a795c142d1dca3adef3c36f9ebc46
Vaharsolta <vaha-22@mail.ru>
2019-12-09 23:13:20 +0300

    Added migration for table moves

---
+++ database/migrations/2019_12_09_193420_create_moves_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moves', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('move');
+            $table->timestamp('play_time');
+            $table->timestamp('play_time_start');
+            $table->char('status', '1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moves');
+    }
+}
