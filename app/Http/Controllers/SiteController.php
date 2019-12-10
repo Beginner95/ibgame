23fe024257403da25f0e8b34eabd179c52f76118
Vaharsolta <vaha-22@mail.ru>
2019-12-10 20:56:34 +0300

    Added base SiteController

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SiteController extends Controller
+{
+    protected $t_rep;
+    protected $e_rep;
+    protected $m_rep;
+    protected $template;
+    protected $vars;
+    protected $contentRightBar = false;
+    protected $bar = false;
+
+        
+
+}
