e3370d14ade5741bff2f1e304213934e155a8fb4
Vaharsolta <vaha-22@mail.ru>
2019-12-11 15:21:37 +0300

    Added relationships hasMany

--- app/Team.php
+++ app/Team.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    public function resources()
+    {
+        return $this->hasMany(Resource::class);
+    }
+
+    public function triggers()
+    {
+        return $this->hasMany(Trigger::class);
+    }
+
+    public function evidence()
+    {
+        return $this->hasMany(Evidence::class);
+    }
 }
