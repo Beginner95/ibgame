ae0c28f9fad6b851ab2d0fa74fd2758a10402af1
Vaharsolta <vaha-22@mail.ru>
2019-12-12 20:59:35 +0300

    Added files for template

---
+++ resources/views/ibgame/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- scripts -->
+<script
+        src="https://code.jquery.com/jquery-3.4.0.min.js"
+        integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg="
+        crossorigin="anonymous">
+</script> <!-- jQuery -->
+<script src='{{ asset(env('THEME')) }}/js/jquery.vide.js'></script>
+<script src="{{ asset(env('THEME')) }}/js/main.js"></script> <!-- main scripts -->
+<!-- scripts -->
+</body>
+</html>
