6edb14872a7b66e61c724bcbbfd84b4734ad2b80
Vaharsolta <vaha-22@mail.ru>
2019-12-12 21:19:12 +0300

    Added head template

---
+++ resources/views/ibgame/layouts/head.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang='ru'>
+<head>
+    <title>Игра</title>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap&subset=cyrillic" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset(env('THEME')) }}/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset(env('THEME')) }}/css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset(env('THEME')) }}/css/media.css">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body>
