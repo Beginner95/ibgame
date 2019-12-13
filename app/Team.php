41085a6a26c9f5cef5409ed05fe7fc594ebccce2
Vaharsolta <vaha-22@mail.ru>
2019-12-13 13:58:57 +0300

    Added sorts

--- app/Team.php
+++ app/Team.php
@@ -8,17 +8,17 @@ class Team extends Model
 {
     public function resources()
     {
-        return $this->hasMany(Resource::class);
+        return $this->hasMany(Resource::class)->orderBy('id', 'DESC');
     }
 
     public function triggers()
     {
-        return $this->hasMany(Trigger::class);
+        return $this->hasMany(Trigger::class)->orderBy('id', 'DESC');
     }
 
     public function evidence()
     {
-        return $this->hasMany(Evidence::class);
+        return $this->hasMany(Evidence::class)->orderBy('id', 'DESC');
     }
 
     public function moves()
