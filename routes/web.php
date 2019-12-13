29f7145066a80037394313779a96663c608eb179
Vaharsolta <vaha-22@mail.ru>
2019-12-13 14:02:02 +0300

    Added routes for game controller

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::resource('/', 'IndexController', [
         'index' => 'home'
     ]
 ]);
+
+Route::get('/game', 'GameController@index')->name('game');
+Route::post('/game/save-time', 'GameController@saveTime')->name('save-time');
+Route::post('/game/answer', 'GameController@answer')->name('answer');
