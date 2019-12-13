5674a4b148503cf868d50efbbbf4c495460eb7f2
Vaharsolta <vaha-22@mail.ru>
2019-12-13 14:14:34 +0300

    Implemented basic game controller

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Answer;
+use App\Move;
+use App\Team;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    public function index(Request $request)
+    {
+        $team_id = $request['team'];
+        $team = Team::where('id', $team_id)->first();
+        $move = Move::where('team_id', $team_id)->where('status', 1)->first();
+
+        $time = [
+            'hour' => Carbon::parse($move->play_time)->format('H'),
+            'minutes' => Carbon::parse($move->play_time)->format('i'),
+            'seconds' => Carbon::parse($move->play_time)->format('s'),
+        ];
+
+        if('0' === $team->status) {
+            $this->edit($team_id);
+        }
+
+        if ((!isset($_COOKIE['game'])) || $_COOKIE['game'] !== 'running' && '1' === $team->status) {
+            return redirect('/');
+        }
+
+        return view(env('THEME') . '.game', [
+            'team' => $team,
+            'time' => $time,
+            'move' => $move
+        ]);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        setcookie('game', 'running', 0);
+        Team::where('id', $id)->update(['status' => 1]);
+        return $_COOKIE['game'] = 'running';
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function saveTime(Request $request)
+    {
+        $team_id = $request['team_id'];
+        $move_id = $request['move_id'];
+        $time = $request['hour'] . ':' . $request['minutes'] . ':' . $request['seconds'];
+        $playTime = Carbon::parse($time)->format('H:i:s');
+        Move::where('id', $move_id)->where('team_id', $team_id)->update(['play_time' => $playTime]);
+    }
+
+    public function answer(Request $request)
+    {
+        $team_id = $request['team-id'];
+        $move_id = $request['move-id'];
+        $answer = $request['answer'];
+
+        if (empty($answer)) return back();
+
+        Move::where('id', $move_id)->where('team_id', $team_id)->update(['status' => 2]);
+        Move::where('team_id', $team_id)->where('status', null)->first()->update(['status' => 1]);
+
+        $a = new Answer();
+        $a->answer = $answer;
+        $a->move_id = $move_id;
+        $a->save();
+
+        return redirect('/game?team=' . $team_id);
+    }
+}
