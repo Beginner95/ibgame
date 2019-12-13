c6bd78427850ff771cd66f16b2da1b70b3303b9c
Vaharsolta <vaha-22@mail.ru>
2019-12-13 16:54:19 +0300

    Added routes for auth

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,9 @@ Route::resource('/', 'IndexController', [
 Route::get('/game', 'GameController@index')->name('game');
 Route::post('/game/save-time', 'GameController@saveTime')->name('save-time');
 Route::post('/game/answer', 'GameController@answer')->name('answer');
+
+Route::group(['namespace' => 'Auth'], function () {
+    Route::get('login', 'LoginController@showLoginForm')->name('login');
+    Route::post('login', 'LoginController@login');
+    Route::get('logout', 'LoginController@logout')->name('logout');
+});
