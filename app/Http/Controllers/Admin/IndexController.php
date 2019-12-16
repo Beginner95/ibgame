b9c17701282fc86003072285109113281ea0d886
Vaharsolta <vaha-22@mail.ru>
2019-12-16 12:57:47 +0300

    Added functions get all resources

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Answer;
+use App\EventOption;
+use App\Evidence;
 use App\Move;
+use App\Resource;
 use App\Team;
+use App\Trigger;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -24,7 +28,11 @@ class IndexController extends Controller
         return view(env('THEME') . '.admin.index', [
             'teams' => $teams,
             'team' => $team,
-            'answer' => $this->getAnswer($team_id)
+            'answer' => $this->getAnswer($team_id),
+            'resources' => $this->getResources(),
+            'evidences' => $this->getEvidences(),
+            'triggers' => $this->getTriggers(),
+            'eventOptions' => $this->getEventOptions()
         ]);
     }
 
@@ -33,5 +41,25 @@ class IndexController extends Controller
         $move = Move::where('status', 2)->where('team_id', $team_id)->first();
         if (empty($move)) return null;
         return Answer::where('move_id', $move->id)->orderBy('id', 'DESC')->first();
+    }
+
+    public function getResources()
+    {
+        return Resource::orderBy('id', 'DESC')->get();
+    }
+
+    public function getEvidences()
+    {
+        return Evidence::orderBy('id', 'DESC')->get();
+    }
+
+    public function getTriggers()
+    {
+        return Trigger::orderBy('id', 'DESC')->get();
+    }
+
+    public function getEventOptions()
+    {
+        return EventOption::get();
     }
 }
