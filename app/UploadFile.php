51ab80318e75f9ca3b9d1ece25b43e38e9e19d20
Vaharsolta <vaha-22@mail.ru>
2019-12-22 07:14:07 +0300

    File upload class implemented

---
+++ app/UploadFile.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vaharsolta
+ * Date: 22.12.2019
+ * Time: 7:03
+ */
+
+namespace App;
+
+use Illuminate\Support\Facades\Input;
+
+
+class UploadFile
+{
+    public $array_path = [
+        'icon' => 'ibgame/img/',
+        'resource' => 'ibgaem/file/resource/',
+        'evidence' => 'ibgame/file/evidence/',
+        'trigger' => 'ibgame/file/trigger/'
+    ];
+
+    public function uploadFile($key)
+    {
+
+        if (Input::file($key)) {
+            $input = Input::file($key);
+            $extension = $input->getClientOriginalExtension();
+            $fileName = rand(11111, 99999) . '.' . $extension;
+            $destinationPath = public_path($this->array_path[$key]);
+            $input->move($destinationPath, $fileName);
+            return $fileName;
+        }
+        return null;
+    }
+
+    public function deleteCurrentImage($currentFile, $key)
+    {
+        if (true === $this->fileExists($currentFile, $key)) {
+            unlink(public_path($this->array_path[$key] . $currentFile));
+        }
+    }
+
+    private  function fileExists($currentFile, $key)
+    {
+        if (!empty($currentFile) && $currentFile != null) {
+            return file_exists(public_path($this->array_path[$key] . $currentFile));
+        }
+    }
+}
