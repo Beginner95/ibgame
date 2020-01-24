cc08f2edddd440fffb44f9bc45b5f67e0ca27568
Vaharsolta <vaha-22@mail.ru>
2020-01-24 16:33:43 +0300

    Added style for login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<div class="container" style="color: black;">
+<div class="container auth">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="modal_heading">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -53,7 +53,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-blue">
                                     {{ __('Login') }}
                                 </button>
 
